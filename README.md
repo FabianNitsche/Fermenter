aa047d85ee7caa67faff996a6911fa5525b14dc6
FabianNitsche <ifab@freenet.de>
2020-04-15 20:55:38 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Fermenter
 This project aims at building an Raspberry Pi controlled Sourdough fermenting box.
+
+The idea is to build a box to control and monitor the fermentation of sourdough.
+
+Controller Equipment:
+- Raspberry Pi Zero W with Raspian Buster Lite and mono-complete (5.18)
+- A SSD1306 monochrom display is connected according to https://indibit.de/raspberry-pi-oled-display-128x64-mit-python-ansteuern-i2c/
+- As a temperature sensor I choose a BMP280, because it is also a humidity sensor.
+
+I searched for quite a while to find a good and simple library to display things and ended up with https://github.com/iot-sas/SSD1306-Sharp which is fast and does not have dependencies. I am adding drawing to the library, so I use my fork to build my sample.
+
+For development of the low level stuff, I can recommend using the VSMonoDebugger: https://marketplace.visualstudio.com/items?itemName=GordianDotNet.VSMonoDebugger0d62
---
+++ SSD1306-Sample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using SSD1306;
+using SSD1306.I2CPI;
+using SSD1306.Fonts;
+
+namespace SSD1306_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var i2cBus = new I2CBusPI("/dev/i2c-1"))
+            {
+                var i2cDevice = new I2CDevicePI(i2cBus, Display.DefaultI2CAddress);
+
+                var display = new SSD1306.Display(i2cDevice, 128, 64);
+                display.Init();
+
+                var dfont = new AdafruitSinglePageFont();
+
+                display.WriteLineBuff(dfont, "Hello World 123456", "Line 2", "Line 3", "Line 4", "Line 5", "Line 6", "Line 7", "Line 8");
+
+                for (int i = 0; i < 100; i++)
+                    display.DrawPixel(i, i);
+
+                display.DisplayUpdate();
+
+            }
+        }
+    }
+}
