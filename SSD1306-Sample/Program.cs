40b87900d2a322f9441c601484eb115e870e9512
FabianNitsche <ifab@freenet.de>
2020-04-16 22:41:34 +0200

    Added button and LEDs

--- SSD1306-Sample/Program.cs
+++ SSD1306-Sample/Program.cs
@@ -26,11 +26,18 @@ namespace SSD1306_Sample
             // Create observable that will generate an incrementing number every second
             var observable = Observable.Generate(1, x => true, x => x + 1, x => x, x => TimeSpan.FromSeconds(1));
 
-            var port = OutputPort.Create(17, OutputPort.InitialValue.Low).Result;
+            var relay = OutputPort.Create(17, OutputPort.InitialValue.Low).Result;
+            var light1 = OutputPort.Create(27, OutputPort.InitialValue.Low).Result;
+            var light2 = OutputPort.Create(22, OutputPort.InitialValue.Low).Result;
+            var button = InputPort.Create(24, GpioEdge.Both).Result;
 
             // Write true whenever the number is even and odd when the number is odd
             using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))
-            using (observable.Select(x => x % 2 == 0).Subscribe(port))
+            //using (observable.Select(x => x % 2 == 0).Subscribe(relay))
+            //using (observable.Select(x => x % 2 == 0).Subscribe(light1))
+            //using (observable.Select(x => x % 2 != 0).Subscribe(light2))
+            //using (button.Do(pressed => Console.WriteLine(pressed)).Subscribe())
+            using (button.Subscribe(light2))
             using (var i2cBus = new I2CBusPI("/dev/i2c-1"))
             {
                 var takePictureTask = cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
@@ -44,22 +51,31 @@ namespace SSD1306_Sample
 
                 var dfont = new AdafruitSinglePageFont();
 
-                for (int i = 0; i < 100; i++)
+                //using (button.Do(pressed =>
+                //{
+                //    display.WriteLineBuff(dfont, pressed.ToString());
+                //    display.DisplayUpdate();
+                //}).Subscribe())
                 {
-                    display.WriteLineBuff(dfont, $"Temperature: {sensor.ReadTemperature().Result} °C", $"Pressure: {sensor.ReadPressure().Result} Pa", $"Humidity: {sensor.ReadHumidity().Result} %", $"Altitude: {sensor.ReadAltitude().Result} m", "Line 5", "Line 6", "Line 7", "Line 8");
-                    display.DisplayUpdate();
+                    //for (int i = 0; i < 100; i++)
+                    //{
+                    //    display.WriteLineBuff(dfont, $"Temperature: {sensor.ReadTemperature().Result} °C", $"Pressure: {sensor.ReadPressure().Result} Pa", $"Humidity: {sensor.ReadHumidity().Result} %", $"Altitude: {sensor.ReadAltitude().Result} m", "Line 5", "Line 6", "Line 7", "Line 8");
+                    //    display.DisplayUpdate();
+                    //}
+
+                    //for (int i = 0; i < 100; i++)
+                    //    display.DrawPixel(i, i);
+
+                    takePictureTask.Wait();
+                    Thread.Sleep(30000);
+                    display.ClearDisplay();
                 }
-
-                //for (int i = 0; i < 100; i++)
-                //    display.DrawPixel(i, i);
-
-                takePictureTask.Wait();
-
-                display.ClearDisplay();
-
             }
             // releasing relay
-            port.Write(true);
+            relay.Write(true);
+            // turning of light 
+            light1.Write(false);
+            light2.Write(false);
             // Cleanup disposes all unmanaged resources and unloads Broadcom library. To be called when no more processing is to be done
             // on the camera.
             cam.Cleanup();
